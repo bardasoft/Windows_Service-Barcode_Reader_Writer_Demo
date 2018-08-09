d6218e943ee5c7da2ea26e5f2e22fb9f9e5546c3
VintaSoft <alex@vintasoft.com>
2018-08-09 14:38:01 +0300

    C# source codes

--- CSharp/AssemblyInfo.cs
+++ CSharp/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VintaSoft Ltd.")]
 [assembly: AssemblyProduct("VintaSoft Barcode .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2016")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("9.1.4.3")]
+[assembly: AssemblyVersion("10.1.0.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
